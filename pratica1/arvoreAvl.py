4f78ad8f3727eafceaca7bc3d41b1409d82bce64
Giovanni <gioestevam04@gmail.com>
2026-03-16 14:38:29 -0300

    feat: Adiciona a classe ArvoreAVL com método de busca

--- pratica1/arvoreAvl.py
+++ pratica1/arvoreAvl.py
@@ -5,3 +5,20 @@ class NoAVL:
         self.direito = None
         self.altura = 1
 
+class ArvoreAVL:
+    def __init__(self):
+        self.raiz = None
+
+    def buscar(self, noAtual, valorBuscado):
+        if noAtual is None:
+            return False
+
+        if valorBuscado < noAtual.valor:
+            return self.buscar(noAtual.esquerdo, valorBuscado)
+
+        elif valorBuscado > noAtual.valor:
+            return self.buscar(noAtual.direito, valorBuscado)
+
+        else:
+            return True
+
