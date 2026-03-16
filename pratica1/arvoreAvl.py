e881b7b7c97ccffa3a3a60cdd8a7847cbe1ae35b
Giovanni <gioestevam04@gmail.com>
2026-03-16 19:15:02 -0300

    feat: Implementa métodos de rotação e inserção na classe ArvoreAVL

--- pratica1/arvoreAvl.py
+++ pratica1/arvoreAvl.py
@@ -33,3 +33,61 @@ class ArvoreAVL:
             return 0
 
         return self.getAltura(no.esquerdo) - self.getAltura(no.direito)
+
+    def rsd(self, y):
+        x = y.esquerdo
+        subArvoreDir = x.direito
+        x.direito = y
+        y.esquerdo = subArvoreDir
+        y.altura = 1 + max(self.getAltura(y.direito), self.getAltura(y.esquerdo))
+        x.altura = 1 + max(self.getAltura(x.direito), self.getAltura(x.esquerdo))
+        return x
+    
+    def rse(self, y):
+        x = y.direito
+        subArvoreEsq = x.esquerdo
+        x.esquerdo = y
+        y.direito = subArvoreEsq
+        y.altura = 1 + max(self.getAltura(y.direito), self.getAltura(y.esquerdo))
+        x.altura = 1 + max(self.getAltura(x.direito), self.getAltura(x.esquerdo))
+        return x
+    
+    def rde(self, no):
+        no.direito = self.rsd(no.direito)
+        no = self.rse(no)
+        return no
+    
+    def rdd(self, no):
+        no.esquerdo = self.rse(no.esquerdo)
+        no = self.rsd(no)
+        return no
+    
+    def inserir(self, noAtual, valor):
+        if noAtual is None:
+            return NoAVL(valor)
+        
+        if valor < noAtual.valor:
+            noAtual.esquerdo = self.inserir(noAtual.esquerdo, valor)
+
+        elif valor > noAtual.valor:
+            noAtual.direito = self.inserir(noAtual.direito, valor)
+        
+        noAtual.altura = 1 + max(self.getAltura(noAtual.direito), self.getAltura(noAtual.esquerdo))
+
+        fb = self.getFatorBalanceamento(noAtual)
+
+        if fb > 1:
+            if valor < noAtual.esquerdo.valor:
+                return self.rsd(noAtual)
+            
+            elif valor > noAtual.esquerdo.valor:
+                return self.rdd(noAtual)
+            
+        elif fb < -1:
+            if valor > noAtual.direito.valor:
+                return self.rse(noAtual)
+            
+            elif valor < noAtual.direito.valor:
+                return self.rde(noAtual)
+
+        return noAtual        
